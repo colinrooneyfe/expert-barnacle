8aff1d6d2ff8476eccf0b8b42e07c40f45e848c2
Colin Rooney <colin.rooney@flatleyeng.com>
2021-06-01 14:29:01 +0100

    test github user used 2

--- src/main/java/com/osgtools/demo/DemoApplication.java
+++ src/main/java/com/osgtools/demo/DemoApplication.java
@@ -46,7 +46,7 @@ public class DemoApplication {
         return smartvendRepo.findById(2L).toString();
     }
 
-    // minor change to test github user used
+    // minor change to test github user used - again
     private void makeEntityDemo() {
         Customer depuy = new Customer();
         depuy.setAccountNo("J0125");
