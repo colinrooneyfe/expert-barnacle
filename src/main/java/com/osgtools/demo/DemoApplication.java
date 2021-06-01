fc8ef50742c713649718d3e57f977e25ebd530f0
Colin Rooney <colin.rooney@flatleyeng.com>
2021-06-01 14:30:12 +0100

    cleanup

--- src/main/java/com/osgtools/demo/DemoApplication.java
+++ src/main/java/com/osgtools/demo/DemoApplication.java
@@ -46,7 +46,6 @@ public class DemoApplication {
         return smartvendRepo.findById(2L).toString();
     }
 
-    // minor change to test github user used - again
     private void makeEntityDemo() {
         Customer depuy = new Customer();
         depuy.setAccountNo("J0125");
